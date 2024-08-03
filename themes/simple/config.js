ad3b334ad219d815eeadeb46df88d380d6250d64
ShuangyingXu <145777056+ShuangyingXu@users.noreply.github.com>
2024-08-03 15:39:11 +0200

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
   SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>Computational Design and Development<br/>/Robotic Fabrication</div>',
 
-  SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
+  SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '/about',
 
   SIMPLE_POST_AD_ENABLE: process.env.NEXT_PUBLIC_SIMPLE_POST_AD_ENABLE || false, // 文章列表是否插入广告
 
